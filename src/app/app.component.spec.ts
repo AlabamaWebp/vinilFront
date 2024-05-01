ae773d3ad01291de18064a5ab21ef794f0c6b7f9
AlabamaWebp <dimyrp@yandex.ru>
2024-05-01 21:59:28 +0300

    home

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'vinilFront' title`, () => {
+  it(`should have the 'vinilF' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('vinilFront');
+    expect(app.title).toEqual('vinilF');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vinilFront');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vinilF');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './comps/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {component: HomeComponent, path: 'home'},
+    {path: '**', redirectTo: 'home'}
+];
