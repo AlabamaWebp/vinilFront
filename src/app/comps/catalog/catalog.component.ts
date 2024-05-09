0393a3ba28ba19c481e3a2173039263d1141b2e3
AlabamaWebp <dimyrp@yandex.ru>
2024-05-09 14:36:12 +0300

    каталог

--- src/app/comps/catalog/catalog.component.ts
+++ src/app/comps/catalog/catalog.component.ts
@@ -9,28 +9,52 @@ import { Component } from '@angular/core';
 })
 export class CatalogComponent {
 
-  catalog_nav: {name: string, icon: string}[] = [
+  nav: nav_item[] = [
     {
-      name: "Классика", 
+      name: "Классика",
       icon: "Ikonka_klassika.png"
     },
     {
-      name: "Рок", 
+      name: "Рок",
       icon: "Ikonka_rok.png"
     },
     {
-      name: "Джаз", 
+      name: "Джаз",
       icon: "ikonka_dzhaz.png"
     },
     {
-      name: "Поп", 
+      name: "Поп",
       icon: "Ikonka_pop.png"
     },
     {
-      name: "Блюз", 
+      name: "Блюз",
       icon: "ikonka_blyuz.png"
     },
   ]
-  current_nav = this.catalog_nav[0];
+  current_nav = this.nav[0];
+  selectNav(item: nav_item) {
+    this.current_nav = item;
+  }
 
+  ngOnInit() {
+    for (let i = 0; i < 10; i++) {
+      this.catalog[i] = {
+        name: "test",
+        img: "test",
+        desc: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima delectus, impedit expedita, at nobis maiores autem a voluptate, doloribus dicta fugiat aliquid consequuntur? Numquam, dolorum est! Atque, molestiae voluptas?",
+      }
+    }
+  }
+  catalog: catalog_item[] = [
+
+  ]
+}
+
+interface nav_item {
+  name: string, icon: string
+}
+interface catalog_item {
+  name: string,
+  img: string,
+  desc: string
 }
